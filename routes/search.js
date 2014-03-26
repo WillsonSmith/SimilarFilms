40bae167b3e8fdf752981d4f99b6989ab43ac5be
Willson Smith <willsonsmth@gmail.com>
2014-03-26 19:31:17 -0400

    oops

--- routes/search.js
+++ routes/search.js
@@ -5,6 +5,6 @@ var getData = require('./getData.js').getData;
 exports.list = function(req, res){
   //homePage();
   var query = req.params.query;
-  getData({title:"index", path: '/3/search/movie?query=' + encodeURIComponent(query) + '&api_key=' + apiKey}, req, res, query);
+  getData({view: "index", title:"search", path: '/3/search/movie?query=' + encodeURIComponent(query) + '&api_key=' + apiKey}, req, res, query);
   //res.render('index', { title: 'Express' });
 };
